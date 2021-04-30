b9905acdeb47b3ff4cda442661f4ff0dfdf44831
Tergel <tergel0666@gmail.com>
2021-04-30 21:04:53 +0800

    Event Listener uuseglee

---
+++ app.js
@@ -0,0 +1,34 @@
+// Working UI
+var uiController = (function(){
+    
+})();
+
+// Working finance controller
+var financeController = (function(){
+    
+})();
+
+// Program combine controller
+var appController = (function(uiController, fnController){
+    var ctrlAdditem = function(){
+
+        // 1.Searching entry data
+        
+        // 2.Transfer data to finance controller
+        
+        // 3.Olson ugugdluude web deere tohiroh eshiig shalgah
+        
+        // 4.Tuswiig tootsoolno
+        
+        // 5.Etsiin uldegdel
+    };
+    document.querySelector(".add__btn").addEventListener("click", function(){
+        ctrlAdditem();
+    });
+
+    document.addEventListener = ("keypress", function(event){
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+})(uiController, financeController);
